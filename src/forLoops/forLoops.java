bd1d0feda755d6bddf6751b350bd0fde771e8381
amelia.brown <amelia.brown@MU-C1-09.mullen.pvt.k12.co.us>
2018-08-27 08:45:58 -0600

    finished number two

---
+++ src/forLoops/forLoops.java
@@ -0,0 +1,19 @@
+package forLoops;
+
+public class forLoops {
+
+	public static void main(String[] args) {
+	challengeOne();
+	challengeTwo();
+	}
+
+	public static void challengeOne(){
+		for (int i = 1; i<6; i++)
+			System.out.println(i);
+	}
+	
+	public static void challengeTwo(){
+		for (int i = 2; i<=10; i = i +2)
+			System.out.println("I love Mullen");
+	}
+}
