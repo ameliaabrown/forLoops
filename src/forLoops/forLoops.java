17bfd2a62cf491748656c066821effd4e236654b
amelia.brown <amelia.brown@MU-C1-09.mullen.pvt.k12.co.us>
2018-08-27 08:56:42 -0600

    finished challenge four

--- src/forLoops/forLoops.java
+++ src/forLoops/forLoops.java
@@ -3,8 +3,11 @@ package forLoops;
 public class forLoops {
 
 	public static void main(String[] args) {
-	challengeOne();
-	challengeTwo();
+	//challengeOne();
+	//challengeTwo();
+	//challengeThree();
+	//challengeFour();
+	challengeFive();
 	}
 
 	public static void challengeOne(){
@@ -15,5 +18,21 @@ public class forLoops {
 	public static void challengeTwo(){
 		for (int i = 2; i<=10; i = i +2)
 			System.out.println("I love Mullen");
+	}
+	
+	public static void challengeThree(){
+		for (int i = 1; i<6; i++)
+			System.out.print(i);
+	}
+	
+	public static void challengeFour(){
+		for (int i =10; i>0; i--)
+			System.out.println(i);
+		System.out.println("Liftoff!");
+	}
+	
+	public static void challengeFive(){
+		for (int i =5; i<=100; i=i+5)
+			System.out.println(i);
 	}
 }
